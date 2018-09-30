7def6aff47e95d7fb2dc9c4736f8aff446bc3d0c
Unknown <photo.karen@gmail.com>
2018-09-29 22:45:31 -0700

    added start screen and start scene

---
+++ HeartsPuzzle/Assets/Scripts/resizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resizer : MonoBehaviour {
+    public Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        float worldScreenHeight = (float)(cam.orthographicSize * 2.0);
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        float widthLocalScale = (worldScreenWidth / GetComponent<SpriteRenderer>().sprite.bounds.size.x);
+        float heightLocalScale = (worldScreenHeight / GetComponent<SpriteRenderer>().sprite.bounds.size.y);
+        transform.localScale = new Vector3(heightLocalScale, heightLocalScale, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
