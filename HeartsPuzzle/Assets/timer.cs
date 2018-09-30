cf3ee0429303bbd0e590e0e5b49a928b3d82b160
Unknown <photo.karen@gmail.com>
2018-09-29 17:19:15 -0700

    more fleshed out script

---
+++ HeartsPuzzle/Assets/timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    public float timeLeft = 300;
+    public Text timerTextBox; 
+
+    public Sprite gameOver;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+
+        if(timeLeft<0)
+        {
+            
+        }
+	}
+}
