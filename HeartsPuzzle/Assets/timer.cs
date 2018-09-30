5a936f79c2fd4497c368d8d38c97240a6df0802e
Unknown <photo.karen@gmail.com>
2018-09-29 19:42:30 -0700

    timer counts down (Brian put the fear of god into the Unity project)

--- HeartsPuzzle/Assets/timer.cs
+++ HeartsPuzzle/Assets/timer.cs
@@ -6,18 +6,21 @@ using UnityEngine.UI;
 public class timer : MonoBehaviour {
 
     public float timeLeft = 300;
-    public Text timerTextBox; 
+    public Text text; 
 
     public Sprite gameOver;
 
 	// Use this for initialization
 	void Start () {
+//        text = GameObject.FindWithTag("timer");
+//        text = GetComponent<Text>;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         timeLeft -= Time.deltaTime;
+        text.text = Mathf.Round(timeLeft).ToString();
 
         if(timeLeft<0)
         {
